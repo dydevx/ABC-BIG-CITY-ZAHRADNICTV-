518e75e4c26ba2f290791649eb748ec4c0c2fd83
dydevx <laitanduylqd02@gmail.com>
2026-06-18 00:25:36 +0700

    upload

--- script.js
+++ script.js
@@ -493,7 +493,7 @@ function initAnimations() {
     entries.forEach((entry) => {
       if (entry.isIntersecting) entry.target.classList.add("visible");
     });
-  }, { threshold: 0.12 });
+  }, { threshold: 0.01, rootMargin: "0px 0px -40px 0px" });
   document.querySelectorAll(".fade-in").forEach((item) => observer.observe(item));
 }
 
